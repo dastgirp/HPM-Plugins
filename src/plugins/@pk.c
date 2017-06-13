9d7f980a97ae97b66df2d005a2c8cb807b1d35c0
Dastgir <dastgirp@gmail.com>
2017-06-13 10:37:57 +0530

    pk: Added 2 configs: enable_maps: config restricting the usage of @pk command on towns/fields pk_maps: config restricting pk'ing on towns/fields

--- src/plugins/@pk.c
+++ src/plugins/@pk.c
@@ -5,7 +5,7 @@
 //===== Modified By: =========================================
 //= Dastgir
 //===== Current Version: =====================================
-//= v1.2
+//= v1.3
 //===== Compatible With: ===================================== 
 //= Hercules
 //===== Description: =========================================
@@ -33,11 +33,23 @@
 HPExport struct hplugin_info pinfo = {
 	"@pk",
 	SERVER_TYPE_MAP,
-	"1.2",
+	"1.3",
 	HPM_VERSION,
 };
 
+// bitwise checking
+enum {
+	PK_ENABLE_TOWN = 1,
+	PK_ENABLE_OTHER = 2,
+	PK_ENABLE_ALL = 3,
+};
+
 int config_delay = 5; // After turn pk on/off, how many seconds delay before the player allow to pk on/off ?
+
+// maps where @pk command can be used
+int enable_maps = PK_ENABLE_TOWN;
+// Which map player can actually pk
+int pk_maps = PK_ENABLE_OTHER;
 
 struct player_data {
 	unsigned int pkmode :1;
@@ -53,8 +65,12 @@ ACMD(pk)
 		ssd->pkmode = 0;
 		addToMSD(sd, ssd, 0, true);
 	}
-	if (!map->list[sd->bl.m].flag.town) {
+	if (enable_maps&PK_ENABLE_TOWN && !map->list[sd->bl.m].flag.town) {
 		clif->message(sd->fd, "You can only change your PK state in towns.");
+		return false;
+	}
+	if (enable_maps&PK_ENABLE_OTHER && map->list[sd->bl.m].flag.town) {
+		clif->message(sd->fd, "You cannot change your PK state in towns.");
 		return false;
 	}
 	if (ssd->pkmode_delay + config_delay > (int)time(NULL)) {
@@ -76,10 +92,22 @@ ACMD(pk)
 int battle_check_target_post(int retVal, struct block_list *src, struct block_list *target, int flag)
 {
 	if (retVal != 1 && src->type == BL_PC && target->type == BL_PC) {
-		struct player_data *src_pc = getFromMSD(BL_CAST(BL_PC, src), 0 );
-		struct player_data *target_pc = getFromMSD(BL_CAST(BL_PC, target), 0 );
-		if (src_pc != NULL && target_pc != NULL && src_pc->status.account_id != target_pc->status.account_id) {
-			if (src_pc->pkmode && target_pc->pkmode) {
+		struct map_session_data *sd = BL_CAST(BL_PC, src);
+		struct map_session_data *targetsd = BL_CAST(BL_PC, target);
+
+		// Can only PK in town
+		if (pk_maps&PK_ENABLE_TOWN && !map->list[sd->bl.m].flag.town) {
+			return retVal;
+		}
+		// Can only pk in fields
+		if (pk_maps&PK_ENABLE_OTHER && map->list[sd->bl.m].flag.town) {
+			return retVal;
+		}
+
+		if (sd->status.account_id != targetsd->status.account_id) {
+			struct player_data *src_pc = getFromMSD(sd, 0);
+			struct player_data *target_pc = getFromMSD(targetsd, 0);
+			if (src_pc != NULL && target_pc != NULL && src_pc->pkmode && target_pc->pkmode) {
 				hookStop();
 				return 1;
 			}
