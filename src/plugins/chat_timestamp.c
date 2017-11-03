739e011b878009d5b2165667eee3ab9b158e199c
Dastgir <dastgirp@gmail.com>
2017-11-03 21:09:34 +0530

    Updated chat_timestamp. Now Supports Whisper. Centralized get_timestamp.

--- src/plugins/chat_timestamp.c
+++ src/plugins/chat_timestamp.c
@@ -3,14 +3,16 @@
 //===== By: ==================================================
 //= Dastgir/Hercules
 //===== Current Version: =====================================
-//= 1.0
+//= 1.1
 //===== Description: =========================================
 //= Will add Timestamp to Party/Guild Chats
 //===== Changelog: ===========================================
 //= v1.0 - Initial Release.
+//= v1.1 - Timestamp can be added to whisper. Fixed PM to NPC.
 //===== Repo Link: ===========================================
 //= https://github.com/dastgir/HPM-Plugins
 //============================================================
+
 #include "common/hercules.h"
 
 #include <stdio.h>
@@ -22,6 +24,7 @@
 #include "common/mmo.h"
 #include "common/nullpo.h"
 #include "common/strlib.h"
+#include "common/socket.h"
 
 #include "map/clif.h"
 #include "map/guild.h"
@@ -36,56 +39,69 @@
  * Enables TimeStamp in Player Messages.
  * This Options Enables Timestamp everywhere a player can send message to.
  **/
-// #define PLAYER_MESSAGE_TIMESTAMP
+//#define ALL_MESSAGE_TIMESTAMP
+// Timestamp for Whispers
+// Message Appears as "Name: [Time] Message"
+//#define WHISPER_TIMESTAMP
+// Timestamp for Party Chats
+#define PARTY_MESSAGE_TIMESTAMP
+// Timestamp for Guild Chats
+#define GUILD_MESSAGE_TIMESTAMP
 
 HPExport struct hplugin_info pinfo =
 {
 	"Chat TimeStamp",
 	SERVER_TYPE_MAP,
-	"1.0",
+	"1.1",
 	HPM_VERSION,
 };
 
-char prefix_msg[CHAT_SIZE_MAX + NAME_LENGTH + 3 + 1];
+// Don't touch below this
+#ifdef ALL_MESSAGE_TIMESTAMP
+	#undef PARTY_MESSAGE_TIMESTAMP
+	#undef GUILD_MESSAGE_TIMESTAMP
+	// Whisper is handled in different function
+	#ifndef WHISPER_TIMESTAMP
+		#define WHISPER_TIMESTAMP
+	#endif
+#endif
+
+void get_timestamp(char* string)
+{
+	time_t t;
+	t = time(NULL);
+	strftime(string, sizeof(string), "[%H:%M] ", localtime(&t));
+	return;
+}
 
 const char *clif_process_chat_message_post(const char *retVal, struct map_session_data *sd, const struct packet_chat_message *packet, char *out_buf, int out_buflen)
 {
-	time_t t;
-	int textlen = 0;
+	char time_prefix[CHAT_SIZE_MAX + NAME_LENGTH + 3 + 1 + 10];
 	
 	nullpo_ret(sd);
 
 	if (retVal == NULL)
 		return NULL;
-#if PACKETVER >= 20151001
-	// Packet doesn't include a NUL terminator
-	textlen = packet->packet_len - 4;
-#else // PACKETVER < 20151001
-	// Packet includes a NUL terminator
-	textlen = packet->packet_len - 4 - 1;
-#endif // PACKETVER > 20151001
-	safestrncpy(out_buf, retVal, textlen+1); // [!] packet->message is not necessarily NUL terminated
 	
-	t = time(NULL);
-	strftime(prefix_msg, 10, "[%H:%M] ", localtime(&t));
-	
-	strcat(prefix_msg, out_buf);
-	retVal = prefix_msg;
+	get_timestamp(time_prefix);
+
+	strcat(time_prefix, out_buf);
+	safestrncpy(out_buf, time_prefix, strlen(time_prefix)+1);
+
+	retVal = out_buf;
 	return retVal;
 }
 
 int guild_send_message_pre(struct map_session_data **sd, const char **mes)
 {
 	char prefix[CHAT_SIZE_MAX + NAME_LENGTH + 3 + 1];
-	time_t t;
 
 	nullpo_ret(*sd);
 
 	if ((*sd)->status.guild_id == 0)
 		return 0;
 	
-	t = time(NULL);
-	strftime(prefix, 10, "[%H:%M] ", localtime(&t));
+	get_timestamp(prefix);
 	
 	strcat(prefix, *mes);
 	
@@ -97,15 +113,13 @@ int guild_send_message_pre(struct map_session_data **sd, const char **mes)
 int party_send_message_pre(struct map_session_data **sd, const char **mes)
 {
 	char prefix[CHAT_SIZE_MAX + NAME_LENGTH + 3 + 1];
-	time_t t;
 
 	nullpo_ret(*sd);
 
 	if ((*sd)->status.party_id == 0)
 		return 0;
 	
-	t = time(NULL);
-	strftime(prefix, 10, "[%H:%M] ", localtime(&t));
+	get_timestamp(prefix);
 	
 	strcat(prefix, *mes);
 	
@@ -113,13 +127,62 @@ int party_send_message_pre(struct map_session_data **sd, const char **mes)
 	return 1;
 }
 
+/// Whisper is transmitted to the destination player (ZC_WHISPER).
+/// 0097 <packet len>.W <nick>.24B <message>.?B
+/// 0097 <packet len>.W <nick>.24B <isAdmin>.L <message>.?B (PACKETVER >= 20091104)
+void clif_wis_message_pre(int *fd_, const char **nick_, const char **mes_, int *mes_len_)
+{
+	char prefix[CHAT_SIZE_MAX + NAME_LENGTH + 3 + 1];
+	int fd = *fd_, mes_len = *mes_len_;
+	const char *nick = *nick_, *mes = *mes_;
+#if PACKETVER >= 20091104
+	struct map_session_data *ssd = NULL;
+#endif // PACKETVER >= 20091104
+	nullpo_retv(nick);
+	nullpo_retv(mes);
+
+	get_timestamp(prefix);
+
+	mes_len += strlen(prefix);
+
+	strcat(prefix, mes);
+
+	mes = (const char*)prefix;
+
+#if PACKETVER < 20091104
+	WFIFOHEAD(fd, mes_len + NAME_LENGTH + 5);
+	WFIFOW(fd,0) = 0x97;
+	WFIFOW(fd,2) = mes_len + NAME_LENGTH + 5;
+	safestrncpy(WFIFOP(fd,4), nick, NAME_LENGTH);
+	safestrncpy(WFIFOP(fd,28), mes, mes_len + 1);
+	WFIFOSET(fd,WFIFOW(fd,2));
+#else
+	ssd = map->nick2sd(nick);
+
+	WFIFOHEAD(fd, mes_len + NAME_LENGTH + 9);
+	WFIFOW(fd,0) = 0x97;
+	WFIFOW(fd,2) = mes_len + NAME_LENGTH + 9;
+	safestrncpy(WFIFOP(fd,4), nick, NAME_LENGTH);
+	WFIFOL(fd,28) = (ssd && pc_get_group_level(ssd) == 99) ? 1 : 0; // isAdmin; if nonzero, also displays text above char
+	safestrncpy(WFIFOP(fd,32), mes, mes_len + 1);
+	WFIFOSET(fd,WFIFOW(fd,2));
+#endif
+	hookStop();
+}
+
 HPExport void plugin_init(void)
 {
-#ifndef PLAYER_MESSAGE_TIMESTAMP
+#ifdef PARTY_MESSAGE_TIMESTAMP
 	addHookPre(party, send_message, party_send_message_pre);
+#endif
+#ifdef GUILD_MESSAGE_TIMESTAMP
 	addHookPre(guild, send_message, guild_send_message_pre);
-#else
+#endif
+#ifdef ALL_MESSAGE_TIMESTAMP
 	addHookPost(clif, process_chat_message, clif_process_chat_message_post);
+#endif
+#ifdef WHISPER_TIMESTAMP
+	addHookPre(clif, wis_message, clif_wis_message_pre);
 #endif
 }
 
