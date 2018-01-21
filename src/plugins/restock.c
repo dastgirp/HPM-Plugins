d110159eedf833539ef46a020969f2cd8efc397d
Dastgir <dastgirp@gmail.com>
2018-01-21 16:46:34 +0530

    Added Storage Expansion Plugin: 	* Allows Unlimited Storage size

--- src/plugins/restock.c
+++ src/plugins/restock.c
@@ -3,14 +3,26 @@
 //===== By: ==================================================
 //= Dastgir/Hercules
 //===== Current Version: =====================================
-//= 1.2
+//= 2.0
 //===== Compatible With: ===================================== 
 //= Hercules
 //===== Description: =========================================
 //= Enables Restock of Items.
 //===== Additional Information: ==============================
-//= Please Load NPC- Restock.txt too.
+//= Requires "Restock.txt" to be loaded too.
 //============================================================
+/*
+
+CREATE TABLE IF NOT EXISTS `restock` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `char_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `restock_from` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `char_id` (`char_id`,`item_id`,`quantity`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+*/
 
 #include "common/hercules.h"
 
@@ -40,148 +52,475 @@
 #include "map/guild.h"
 
 #include "plugins/HPMHooking.h"
-#include "common/HPMDataCheck.h" /* should always be the last file included! (if you don't make it last, it'll intentionally break compile time) */
+#include "common/HPMDataCheck.h"
 
 HPExport struct hplugin_info pinfo = {
 	"Restock System",
 	SERVER_TYPE_MAP,
-	"1.2",
+	"2.0",
 	HPM_VERSION,
 };
 
-int restock_misc_itemid;
+struct restock_item {
+	int id;
+	unsigned short quantity;
+	short restock_from;
+};
+
+struct restock_data {
+	int idx;
+	bool enabled;
+	VECTOR_DECL(struct restock_item) restock;
+};
+
+enum { // restock_from
+	RS_STORAGE = 0,
+	RS_GSTORAGE,
+	RS_MAX,
+};
+
+enum {
+	RS_TYPE_ADD = 0,
+	RS_TYPE_DEL,
+	RS_TYPE_DEL_ALL,
+	RS_TYPE_MAX,
+};
+
+bool rs_exists(struct map_session_data *sd)
+{
+	return (getFromMSD(sd, 0) == NULL) ? false : true;
+}
+
+
+struct restock_data* rs_search(struct map_session_data *sd, bool create)
+{
+	struct restock_data *data = NULL;
+	if ((data = getFromMSD(sd, 0)) == NULL && create) {
+		CREATE(data, struct restock_data, 1);
+		data->idx = -1;
+		data->enabled = false;
+		addToMSD(sd, data, 0, true);
+	}
+	return data;
+}
 
 int pc_restock_misc_pre(struct map_session_data **sd, int *n, int *amount, int *type, short *reason, e_log_pick_type* log_type)
 {
 	int index = *n;
+	struct restock_data *rsd;
 	if (*sd == NULL)
 		return 1;
-	if (pc_readglobalreg(*sd, script->add_str("restk")) == 1) {
-		restock_misc_itemid = 0;
+
+	rsd = rs_search(*sd, false);
+
+	if (rsd != NULL && rsd->enabled) {
 		if ((*sd)->status.inventory[index].nameid > 0){
-			restock_misc_itemid = (*sd)->status.inventory[index].nameid;
+			int i, id = (*sd)->status.inventory[index].nameid;
+			ARR_FIND(0, VECTOR_LENGTH(rsd->restock), i, VECTOR_INDEX(rsd->restock, i).id == id);
+
+			if (i != VECTOR_LENGTH(rsd->restock)) {
+				rsd->idx = i;
+			}
 		}
 	}
 	return 0;
 }
 
+
 int pc_restock_misc_post(int retVal, struct map_session_data *sd, int n, int amount, int type, short reason, e_log_pick_type log_type)
 {
+	struct restock_data *rsd;
 	if (retVal == 1)
 		return retVal;
 
-	if (pc_readglobalreg(sd, script->add_str("restk")) == 1 && restock_misc_itemid && pc->search_inventory(sd, restock_misc_itemid) == -1){
-		pc_setglobalreg(sd,script->add_str("restkid"), restock_misc_itemid );
-		npc->event(sd, "Restock::OnRestock", 0);
-		restock_misc_itemid = 0;
+	rsd = rs_search(*sd, false);
+
+	if (rsd != NULL && rsd->enabled && rsd->idx > -1) {
+		struct restock_item rsi = VECTOR_INDEX(rsd->restock, i);
+		if (pc->search_inventory(sd, rsi.id) < rsi.quantity) {
+			restock(sd, rsi.id, rsi.quantity, rsi.restock_from);
+		}
+		rsd->idx = -1;
 	}
 	return retVal;
 }
 
-BUILDIN(restock_item)
+
+const char* restock_from_name(int rs_from)
 {
-	int rid, rqu, fr;
-	int i, j, flag;
-	struct map_session_data *sd;
-	struct item item_tmp;
-	bool success = false;
-	rid = script_getnum(st, 2);
-	rqu = script_getnum(st, 3);
-	fr = script_getnum(st, 4);
-	sd = script->rid2sd(st);
+	switch (rs_from) {
+		case RS_STORAGE:
+			return "Storage";
+		case RS_GSTORAGE:
+			return "GStorage";
+	}
+	return "Unknown";
+}
 
-	if (sd == NULL) {
-		script_pushint(st, 0);
-		return false;
-	} else if (fr == 2) {
-		struct guild *g;
-		struct guild_storage *gstorage2;
-		g = guild->search(sd->status.guild_id);
-		if (g == NULL) {
-			clif->message(sd->fd, msg_txt(43));
-			script_pushint(st, 0);
-			return true;
-		}
-		gstorage2 = gstorage->ensure(sd->status.guild_id);
-		if (gstorage == NULL) {// Doesn't have opened @gstorage yet, so we skip the deletion since *shouldn't* have any item there.
-			script_pushint(st, 0);
-			return true;
-		}
-		j = gstorage2->storage_amount;
-		gstorage2->lock = 1; 
-		for (i = 0; i < j; ++i) {
-			if (gstorage2->items[i].nameid == rid && gstorage2->items[i].amount >= rqu) {
-				memset(&item_tmp, 0, sizeof(item_tmp));
-				item_tmp.nameid = gstorage2->items[i].nameid;
-				item_tmp.identify = 1;
-				gstorage->delitem(sd, gstorage2, i, rqu);
-				if ((flag = pc->additem(sd,&item_tmp,rqu,LOG_TYPE_STORAGE))) {
-					clif->additem(sd, 0, 0, flag);
-					pc_setglobalreg(sd,script->add_str("restkid1"), gstorage2->items[i].nameid);
-					pc_setglobalreg(sd,script->add_str("restkid2"), rqu);
-					gstorage->close(sd);
-					gstorage2->lock = 0;
-					success = true;
-					script_pushint(st, 1);
-					break;
-				}
-			}
-		}
-		gstorage->close(sd);
-		gstorage2->lock = 0;
-	} else if (fr == 1) {
-		if (sd->state.storage_flag == STORAGE_FLAG_NORMAL) {
-			sd->state.storage_flag = STORAGE_FLAG_CLOSED;
-			storage->close(sd);
-		}
+bool resotck_add_item(struct map_session_data *sd, int item_id, int quantity, int rs_from)
+{
+	int i;
+	bool first_time = false;
+	struct restock_data *rsd = NULL;
+	struct restock_item *rsi = NULL;
 
-		if (sd->storage.received == false) {
-			clif->message(sd->fd, msg_txt(27)); // "Storage has not been loaded yet"
-			script_pushint(st, 0);
-			return true;
-		}
-		
-		j = VECTOR_LENGTH(sd->storage.item);
-		sd->state.storage_flag = STORAGE_FLAG_NORMAL;
+	if (!rs_exists(sd)) {
+		first_time = true;
+	}
+	rsd = rs_search(sd, true);
 
-		for (i = 0; i < VECTOR_LENGTH(sd->storage.item); ++i) {
-			if (VECTOR_INDEX(sd->storage.item, i).nameid == rid && VECTOR_INDEX(sd->storage.item, i).amount >= rqu) {
-				memset(&item_tmp, 0, sizeof(item_tmp));
-				item_tmp.nameid = rid;
-				item_tmp.identify = 1;
-
-				storage->delitem(sd, i, VECTOR_INDEX(sd->storage.item, i).amount);
-
-				if ((flag = pc->additem(sd, &item_tmp, rqu, LOG_TYPE_STORAGE))) {
-					clif->additem(sd, 0, 0, flag);
-					pc_setglobalreg(sd,script->add_str("restkid1"), rid);
-					pc_setglobalreg(sd,script->add_str("restkid2"), rqu);
-					success = true;
-					break;
-				}
-			}
-		}
-		sd->state.storage_flag = STORAGE_FLAG_CLOSED;
-		storage->close(sd);
+	if (first_time) {
+		VECTOR_INIT(rsd->restock);
 	}
 
-	if (!success) {
-		script_pushint(st, 0);
+	ARR_FIND(0, VECTOR_LENGTH(rsd->restock), i, VECTOR_INDEX(rsd->restock, i).id == item_id);
+	// New restock id.
+	if (i == VECTOR_LENGTH(rsd->restock)) {
+		VECTOR_PUSHZEROED(rsd->restock, 1, 1);
+		rsi = &VECTOR_LAST(rsd->restock);
 	} else {
-		script_pushint(st, 1);
+		rsi = &VECTOR_INDEX(rsd->restock, i);
+	}
+	rsi->id = item_id;
+	rsi->quantity = quantity;
+	rsi->restock_from = rs_from;
+
+	if (SQL_ERROR == SQL->Query(map->mysql_handle,
+		"INSERT INTO `restock` (`char_id`, `item_id`, `quantity`, `restock_from`) VALUES ('%d', '%d', '%d', '%d') 
+			ON DUPLICATE KEY UPDATE `quantity`='%d', `restock_from`='%d'",
+								sd->status.char_id,
+								item_id,
+								quantity,
+								rs_from,
+								quantity,
+								rs_from
+								))
+		Sql_ShowDebug(map->mysql_handle);
+}
+
+bool restock(struct map_session_data *sd, int item_id, int quantity, int rs_from)
+{
+	int i, j, flag;
+	struct item item_tmp;
+	bool pushed_one = false;
+
+	if (sd == NULL) {
+		return false;
+	}
+	switch (rs_from) {
+		case RS_GSTORAGE: {
+			struct guild *g;
+			struct guild_storage *gstorage2;
+			g = guild->search(sd->status.guild_id);
+			if (g == NULL) {
+				clif->message(sd->fd, msg_txt(43));
+				return false;
+			}
+			gstorage2 = gstorage->ensure(sd->status.guild_id);
+			if (gstorage == NULL) {// Doesn't have opened @gstorage yet, so we skip the deletion since *shouldn't* have any item there.
+				script_pushint(st, 0);
+				return false;
+			}
+			j = gstorage2->storage_amount;
+			gstorage2->lock = 1; 
+			for (i = 0; i < j; ++i) {
+				if (gstorage2->items[i].nameid == item_id && gstorage2->items[i].amount >= quantity) {
+					memset(&item_tmp, 0, sizeof(item_tmp));
+					item_tmp.nameid = gstorage2->items[i].nameid;
+					item_tmp.identify = 1;
+					gstorage->delitem(sd, gstorage2, i, quantity);
+					if ((flag = pc->additem(sd,&item_tmp,quantity,LOG_TYPE_STORAGE))) {
+						clif->additem(sd, 0, 0, flag);
+						gstorage->close(sd);
+						gstorage2->lock = 0;
+						pushed_one = true;
+						break;
+					}
+				}
+			}
+			gstorage->close(sd);
+			gstorage2->lock = 0;
+			break;
+		}
+
+		case RS_STORAGE: {
+			struct storage_data* stor = &sd->status.storage;
+			if (stor == NULL) {
+				return false;
+			}
+			j = stor->storage_amount;
+			if (sd->state.storage_flag) {
+				sd->state.storage_flag = 0;
+				storage->close(sd);
+			}
+			sd->state.storage_flag = 1;
+			for (i = 0; i < j; ++i) {
+				if (stor->items[i].nameid == item_id && stor->items[i].amount >= quantity) {
+					memset(&item_tmp, 0, sizeof(item_tmp));
+					item_tmp.nameid = stor->items[i].nameid;
+					item_tmp.identify = 1;
+					storage->delitem(sd, i, quantity);
+					if ((flag = pc->additem(sd,&item_tmp,quantity,LOG_TYPE_STORAGE))) {
+						clif->additem(sd, 0, 0, flag);
+						sd->state.storage_flag = 0;
+						storage->close(sd);
+						pushed_one = true;
+						break;
+					}
+				}
+			}
+			sd->state.storage_flag = 0;
+			storage->close(sd);
+			break;
+		}
+	}
+
+	if (!pushed_one) {
+		return false;
 	}
 
 	return true;
 }
 
+void pc_load_restock_data(int *fd, struct map_session_data **sd_) {
+	char *data = NULL;
+	struct map_session_data *sd = *sd_;
+
+	if (sd->state.connect_new) {
+		if (SQL_ERROR == SQL->Query(map->mysql_handle,
+			"SELECT `item_id`, `quantity`, `restock_from` FROM `restock` WHERE `char_id` = '%d'", sd->status.char_id))
+			Sql_ShowDebug(map->mysql_handle);
+
+		while (SQL_SUCCESS == SQL->NextRow(map->mysql_handle)) {
+			int id, quantity, rs_from;
+			SQL->GetData(map->mysql_handle, 0, &data, NULL); id = atoi(data);
+			SQL->GetData(map->mysql_handle, 1, &data, NULL); quantity = atoi(data);
+			SQL->GetData(map->mysql_handle, 2, &data, NULL); rs_from = atoi(data);
+			restock_add_item(sd, id, quantity, rs_from);
+			break;
+		}
+		SQL->FreeResult(map->mysql_handle);
+	}
+}
+
+BUILDIN(restock_list)
+{
+	struct restock_data *rsd = NULL;
+	struct map_session_data *sd = script->rid2sd(st);
+	int i;
+	char output[1024];
+
+	if (sd == NULL) {
+		script_pushint(st, 0);
+		return true;
+	}
+
+	if ((rsd = rs_search(sd, false)) == NULL) {
+		script_pushint(st, 0);
+		return true;
+	}
+
+	sprintf(output, "Restock From\tQuantity\tItemID\n");
+	clif->message(sd->fd, output);
+	for (i = 0; i < VECTOR_LENGTH(rsd->restock); i++) {
+		struct restock_item *rsi = &VECTOR_INDEX(rsd->restock, i);
+		sprintf(output, "%d.) %s\t%d\t%d", (i + 1), restock_from_name(rsi->restock_from), rsi->quantity, rsi->id);
+		clif->message(sd->fd, output);
+	}
+
+	script_pushint(st, 1);
+	return true;
+}
+
+/**
+ * restock(<itemID>,<Quantity>,<restock From>,<Add/Remove>)
+ */
+BUILDIN(restock)
+{
+	int item_id = 0;
+	int quantity = script_getnum(st, 3);
+	int rs_from = script_getnum(st, 4);
+	int type = script_getnum(st, 5);
+	struct map_session_data *sd = script->rid2sd(st);
+
+	if (sd == NULL) {
+		script_pushint(st, 0);
+		return false;
+	}
+
+	if (quantity < 0) {
+		clif->message(sd->fd, "restock: quantity should be greater than 0.");
+		script_pushint(st, 0);
+		return true;
+	}
+
+	if (rs_from < 0 || rs_from >= RS_MAX) {
+		clif->message(sd->fd, "restock: Invalid 'restock From' Value.");
+		script_pushint(st, 0);
+		return true;
+	}
+
+	if (type < 0 || type > RS_TYPE_MAX) {
+		clif->message(sd->fd, "restock: Invalid Type mentioned.");
+		script_pushint(st, 0);
+		return true;
+	}
+
+	if (script_isstringtype(st, 2)) {
+		struct item_data* id = itemdb->search_name(script_getstr(st, 2));
+		if (id != NULL) {
+			item_id = id->nameid;
+		}
+	} else {
+		item_id = script_getnum(st, 2);
+		if (itemdb->exists(item_id) == NULL) {
+			item_id = 0;
+		}
+	}
+
+	if (item_id == 0) {
+		ShowError("restock: Invalid ItemID(%d).\n", item_id);
+		script_pushint(st,0);
+		return true;
+	}
+
+	switch (type) {
+		case RS_TYPE_ADD: {
+			restock_add_item(sd, item_id, quantity, rs_from);
+			break;
+		}
+		case RS_TYPE_DEL: {
+			int i;
+			struct restock_data *rsd = NULL;
+			struct restock_item *rsi = NULL;
+			if (!rs_exists(sd)) {
+				clif->message("restock: No Item exists in restock list.");
+				script_pushint(st, 0);
+				return true;
+			}
+			rsd = rs_search(sd, false);
+
+			ARR_FIND(0, VECTOR_LENGTH(rsd->restock), i, VECTOR_INDEX(rsd->restock, i).id == item_id);
+			// Not Found
+			if (i == VECTOR_LENGTH(rsd->restock)) {
+				clif->message("restock: No Such Item exists in restock list.");
+				script_pushint(st, 0);
+				return true;
+			}
+
+			VECTOR_ERASE(rsd->restock, i);
+
+			if (SQL_ERROR == SQL->Query(map->mysql_handle,
+				"DELETE FROM `restock` WHERE `item_id`='%d' AND `char_id`='%d'",
+										item_id,
+										sd->status.char_id
+										))
+				Sql_ShowDebug(map->mysql_handle);
+
+		}
+			break;
+		case RS_TYPE_DEL_ALL: {
+			if (SQL_ERROR == SQL->Query(map->mysql_handle, "DELETE FROM `restock` `char_id`='%d'", sd->status.char_id))
+				Sql_ShowDebug(map->mysql_handle);
+			}
+			break;
+	}
+
+	script_pushint(st, 1);
+	return true;
+}
+
+
+/**
+ * restock_toggle({true/false})
+ * Returns -1 (error), 0(disabled), 1(enabled)
+ */
+BUILDIN(restock_toggle)
+{
+	struct map_session_data *sd = script->rid2sd(st);
+	int toggle = -1;
+	struct restock_data *rsd = NULL;
+
+	if (sd == NULL) {
+		script_pushint(st, -1);
+		return false;
+	}
+
+	if (script_hasdata(st, 2)) {
+		toggle = script_getnum(st, 2);
+		if (toggle < 0 || toggle > 1) {
+			ShowError("restock_toggle: Invalid Toggle Value(%d).\n", toggle);
+			script_pushint(st, -1);
+			return true;
+		}
+	}
+
+	if (toggle == 0 && !rs_exists(sd)) {
+		script_pushint(st, 1);
+		return true;
+	}
+	
+	rsd = rs_search(sd, true);
+
+	if (rsd->enabled && toggle <= 0) {
+		rsd->enabled = false;
+		script_pushint(st, 0);
+	} else if (!rsd->enabled && (toggle == 1 || toggle == -1)) {
+		rsd->enabled = true;
+		script_pushint(st, 1);
+	} else {
+		script_pushint(st, -1);
+	}
+	
+	return true;
+}
+
+BUILDIN(restock_status)
+{
+	struct map_session_data *sd = script->rid2sd(st);
+	struct restock_data *rsd = NULL;
+
+	if (sd == NULL) {
+		script_pushint(st, 0);
+		return false;
+	}
+
+	if (!rs_exists(sd)) {
+		script_pushint(st, 0);
+		return true;
+	}
+	
+	rsd = rs_search(sd, true);
+
+	if (rsd->enabled) {
+		script_pushint(st, 1);
+	} else {
+		script_pushint(st, 0);
+	}
+	
+	return true;
+}
+
+
 HPExport void plugin_init(void)
 {
 	addHookPre(pc, delitem, pc_restock_misc_pre);
 	addHookPost(pc, delitem, pc_restock_misc_post);
-	addScriptCommand("restock_item", "iii", restock_item);
+	addHookPre(clif, pLoadEndAck, pc_load_restock_data);
+	addScriptCommand("restock", "viii", restock);
+	addScriptCommand("restock_list", "", restock_list);
+	addScriptCommand("restock_toggle", "?", restock_toggle);
+	addScriptCommand("restock_status", "", restock_status);
 }
 
 HPExport void server_online(void)
 {
 	ShowInfo("'%s' Plugin by Dastgir/Hercules. Version '%s'\n", pinfo.name, pinfo.version);
+	// Constants
+	script->set_constant("RS_TYPE_ADD", RS_TYPE_ADD, false, false);
+	script->set_constant("RS_TYPE_DEL", RS_TYPE_DEL, false, false);
+	script->set_constant("RS_TYPE_DEL_ALL", RS_TYPE_DEL_ALL, false, false);
+
+	script->set_constant("RS_STORAGE", RS_STORAGE, false, false);
+	script->set_constant("RS_GSTORAGE", RS_GSTORAGE, false, false);
 }
---
+++ src/plugins/storage_expand.c
@@ -0,0 +1,329 @@
+//===== Hercules Plugin ======================================
+//= Storage Expansion
+//===== By: ==================================================
+//= Dastgir/Hercules
+//===== Current Version: =====================================
+//= 1.0
+//===== Description: =========================================
+//= Expands Storage Size.
+//= If this plugin is enabled, Server can have any number of
+//= storage size defined.
+//===== Instruction: =========================================
+//= Enable This Plugin
+//= Change MAX_STORAGE from common/mmo.h
+//= Ensure storage_expansion = true; in this plugin
+//===== Changelog: ===========================================
+//= v1.0 - Initial Conversion
+//===== Repo Link: ===========================================
+//= https://github.com/dastgir/HPM-Plugins
+//===== Hercules Topic: ======================================
+//= http://herc.ws/board/topic/15636-infinite-storage/
+//============================================================
+#include "common/hercules.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "common/HPMi.h"
+#include "common/memmgr.h"
+#include "common/mmo.h"
+#include "common/nullpo.h"
+#include "common/socket.h"
+#include "common/strlib.h"
+#include "map/chrif.h"
+#include "map/pc.h"
+#include "map/map.h"
+#include "map/intif.h"
+#undef DEFAULT_AUTOSAVE_INTERVAL
+#include "char/char.h"
+#include "char/mapif.h"
+#include "char/int_storage.h"
+
+#include "plugins/HPMHooking.h"
+#include "common/HPMDataCheck.h"
+
+HPExport struct hplugin_info pinfo = {
+	"Storage Expansion",
+	SERVER_TYPE_MAP|SERVER_TYPE_CHAR,
+	"1.0",
+	HPM_VERSION,
+};
+
+bool storage_expansion = true;
+
+#define inter_fd (chrif->fd)
+
+struct storage_plugin_data {
+	struct storage_data p_stor;
+	int total_parts, current_part, count;
+};
+
+/**
+ * Send account storage information for saving.
+ * @packet 0x3011 [out] <packet_len>.W <account_id>.L <struct item[]>.P
+ * @param  sd     [in]  pointer to session data.
+ */
+void intif_send_account_storage_pre(const struct map_session_data **sd_)
+{
+	const struct map_session_data *sd = *sd_;
+	int len = 0, i = 0, c = 0;
+	int16 parts = 1;
+	int offset = 12, j = 0, len_t = 0;
+
+	if (!storage_expansion)
+		return;
+	nullpo_retv(sd);
+
+	// Assert that at this point in the code, both flags are true.
+	Assert_retv(sd->storage.save == true);
+	Assert_retv(sd->storage.received == true);
+
+	if (intif->CheckForCharServer())
+		return;
+
+	len = sd->storage.aggregate * sizeof(struct item);
+	if (len == 0)
+		len = 1;
+	parts = ceil(len/65535.0f);
+
+	for (j = 0; j < parts; j++) {
+		len_t = 12;
+		for (c = 0; i < VECTOR_LENGTH(sd->storage.item); i++) {
+			if (VECTOR_INDEX(sd->storage.item, i).nameid == 0)
+				continue;
+			if (len_t + sizeof(struct item) > 65535)
+				break;
+			memcpy(WFIFOP(inter_fd, offset + c * sizeof(struct item)), &VECTOR_INDEX(sd->storage.item, i), sizeof(struct item));
+			len_t += sizeof(struct item);
+			c++;
+		}
+		WFIFOHEAD(inter_fd, len_t);
+
+		WFIFOW(inter_fd, 0) = 0x3011;
+		WFIFOW(inter_fd, 2) = (uint16) len_t;
+		WFIFOL(inter_fd, 4) = sd->status.account_id;
+		WFIFOW(inter_fd, 8) = j;	// Current Part
+		WFIFOW(inter_fd, 10) = parts;	// Total Parts
+
+		WFIFOSET(inter_fd, len_t);
+		//ShowInfo("Sent %d(%d) Items (%d/%d), Length: %d(%d), Expected: %d\n", c, i, j, parts, len_t, len, sd->storage.aggregate);
+	}
+	hookStop();
+}
+
+/**
+ * Parse the reception of account storage from the inter-server.
+ * @packet 0x3805 [in] <packet_len>.W <account_id>.L <struct item[]>.P
+ * @param  fd     [in] file/socket descriptor.
+ */
+void intif_parse_account_storage_pre(int *fd_)
+{
+	int fd = *fd_;
+	int account_id = 0, payload_size = 0, storage_count = 0;
+	int i = 0;
+	int total_parts;
+	struct map_session_data *sd = NULL;
+	struct storage_plugin_data *data;
+
+	Assert_retv(fd > 0);
+
+	if (!storage_expansion)
+		return;
+
+	payload_size = RFIFOW(fd, 2) - 12;
+	total_parts = RFIFOW(fd, 10);
+	//parts = RFIFOW(fd, 8);
+
+	if ((account_id = RFIFOL(fd, 4)) == 0 || (sd = map->id2sd(account_id)) == NULL) {
+		ShowError("intif_parse_account_storage: Session pointer was null for account id %d!\n", account_id);
+		return;
+	}
+
+	if (sd->storage.received == true) {
+		ShowError("intif_parse_account_storage: Multiple calls from the inter-server received.\n");
+		return;
+	}
+
+	if (!(data = getFromSession(sockt->session[fd],1))) {
+		CREATE(data,struct storage_plugin_data,1);
+		addToSession(sockt->session[fd],data,1,true);
+		memset(&data->p_stor, 0, sizeof(data->p_stor));
+		data->total_parts = total_parts;
+		data->current_part = 0;
+		data->count = 0;
+	}
+
+	//ShowInfo("Received %d/%d, Size: %d. AccountID: %d\n", RFIFOW(fd, 8), total_parts, payload_size, account_id);
+
+	storage_count = (payload_size/sizeof(struct item));
+	data->count += storage_count;
+
+	VECTOR_ENSURE(sd->storage.item, data->count, 1);
+
+	sd->storage.aggregate = data->count; // Total items in storage.
+
+	for (i = 0; i < storage_count; i++) {
+		const struct item *it = RFIFOP(fd, 12 + i * sizeof(struct item));
+		VECTOR_PUSH(sd->storage.item, *it);
+	}
+	data->current_part += 1;
+
+	if (data->current_part != data->total_parts) {
+		hookStop();
+		return;
+	}
+
+	sd->storage.received = true; // Mark the storage state as received.
+	sd->storage.save = false; // Initialize the save flag as false.
+
+	pc->checkitem(sd); // re-check remaining items.
+
+	removeFromSession(sockt->session[fd], 1);
+	hookStop();
+}
+
+/**
+ * Parses an account storage save request from the map server.
+ * @packet 0x3011 [in] <packet_len>.W <account_id>.L <struct item[]>.P
+ * @param  fd     [in] file/socket descriptor.
+ * @return 1 on success, 0 on failure.
+ */
+int mapif_parse_AccountStorageSave_pre(int *fd_)
+{
+	int fd = *fd_;
+	int payload_size = RFIFOW(fd, 2) - 12, account_id = RFIFOL(fd, 4);
+	int total_parts = RFIFOW(fd, 10);
+	//int parts = RFIFOW(fd, 8);
+	int i = 0, count = 0;
+	struct storage_plugin_data *data;
+
+	if (!storage_expansion)
+		return 0;
+
+	Assert_ret(fd > 0);
+	Assert_ret(account_id > 0);
+
+	if ((data = getFromSession(sockt->session[fd],0)) == NULL) {
+		CREATE(data,struct storage_plugin_data,1);
+		addToSession(sockt->session[fd],data,0,true);
+		memset(&data->p_stor, 0, sizeof(data->p_stor));
+		data->total_parts = total_parts;
+		data->current_part = 0;
+		data->count = 0;
+		VECTOR_INIT(data->p_stor.item);
+	}
+
+	if (data->total_parts == 0) {
+		ShowError("mapif_storage_plugin: Invalid Total Parts.\n");
+		hookStop();
+		return 0;	
+	}
+
+
+	count = payload_size/sizeof(struct item);
+	data->count += count;
+
+	if (count > 0) {
+		VECTOR_ENSURE(data->p_stor.item, data->count, 1);
+
+		for (i = 0; i < count; i++) {
+			const struct item *it = RFIFOP(fd, 12 + i * sizeof(struct item));
+
+			VECTOR_PUSH(data->p_stor.item, *it);
+		}
+
+		data->p_stor.aggregate += count;
+		//ShowInfo("Recieved (SR) Part: %d/%d Count: %d/%d Size: %d\n", data->current_part, data->total_parts, count, data->count, payload_size);
+		data->current_part += 1;
+	}
+
+	if (data->current_part != data->total_parts) {
+		hookStop();
+		return 1;
+	}
+
+	inter_storage->tosql(account_id, &data->p_stor);
+
+	VECTOR_CLEAR(data->p_stor.item);
+
+	mapif->sAccountStorageSaveAck(fd, account_id, true);
+	removeFromSession(sockt->session[fd], 0);
+	hookStop();
+
+	return 1;
+}
+
+/**
+ * Loads the account storage and send to the map server.
+ * @packet 0x3805     [out] <account_id>.L <struct item[]>.P
+ * @param  fd         [in]  file/socket descriptor.
+ * @param  account_id [in]  account id of the session.
+ * @return 1 on success, 0 on failure.
+ */
+int mapif_account_storage_load_pre(int *fd_, int *account_id_)
+{
+	int fd = *fd_, account_id = *account_id_;
+	struct storage_data stor = { 0 };
+	int count = 0, i = 0, len = 0;
+	int len_t = 0, j = 0, c = 0;
+	int offset = 12;
+	int parts = 0;
+
+	Assert_ret(account_id > 0);
+
+	if (!storage_expansion)
+		return 0;
+
+	VECTOR_INIT(stor.item);
+	count = inter_storage->fromsql(account_id, &stor);
+
+	len = count * sizeof(struct item);
+	if (len == 0)
+		len = 1;
+	parts = ceil(len/65535.0f);
+
+	for (j = 0; j < parts; j++) {
+		len_t = 12;
+		for (c = 0; i < count; i++) {
+			if (len_t + sizeof(struct item) > 65535)
+				break;
+			memcpy(WFIFOP(fd, offset + c * sizeof(struct item)), &VECTOR_INDEX(stor.item, i), sizeof(struct item));
+			len_t += sizeof(struct item);
+			c++;
+		}
+		WFIFOHEAD(fd, len_t);
+
+		WFIFOW(fd, 0) = 0x3805;
+		WFIFOW(fd, 2) = (uint16) len_t;
+		WFIFOL(fd, 4) = account_id;
+		WFIFOW(fd, 8) = j;	// Current Part
+		WFIFOW(fd, 10) = parts;	// Total Parts
+
+		WFIFOSET(fd, len_t);
+		//ShowInfo("Sent %d Items(Storage) Part: %d/%d. Length: %d/%d, Items: %d/%d\n", i, j, parts, len_t, len, c, i);
+	}
+
+
+	VECTOR_CLEAR(stor.item);
+
+	hookStop();
+	return 1;
+}
+
+HPExport void plugin_init(void)
+{
+	if (SERVER_TYPE == SERVER_TYPE_MAP) {
+		addHookPre(intif, send_account_storage, intif_send_account_storage_pre);
+		addHookPre(intif, pAccountStorage, intif_parse_account_storage_pre);
+	} else {
+		addHookPre(mapif, account_storage_load, mapif_account_storage_load_pre);
+		addHookPre(mapif, pAccountStorageSave, mapif_parse_AccountStorageSave_pre);
+	}
+}
+
+HPExport void server_online(void)
+{
+	ShowInfo("'%s' Plugin by Dastgir/Hercules. Version '%s'\n", pinfo.name, pinfo.version);
+}
